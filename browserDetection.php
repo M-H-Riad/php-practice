fb108220e584ef1e4a67f1a9848ec69dabab0ef5
M-H_Riad <meshkathasanriad@gmail.com>
2018-11-09 01:32:14 +0600

    A script for user agent information.

---
+++ browserDetection.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Browser detection</title>
+</head>
+<body>
+	<?php
+
+		echo "User information is : ".$_SERVER ['HTTP_USER_AGENT'];
+
+	?>
+
+</body>
+</html>
